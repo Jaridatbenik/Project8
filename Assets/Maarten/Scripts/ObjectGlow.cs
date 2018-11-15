e0b174c2a72f699dbf392be730688cf7bbd6b309
mfavlutters <mfavlutters@gmail.com>
2018-11-15 08:47:29 +0100

    New scene with object that becomes highlighted when you look at it. (for interacting with objects)

---
+++ Assets/Maarten/Scripts/ObjectGlow.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectGlow : MonoBehaviour {
+
+    Light light;
+
+    // Use this for initialization
+    void Start ()
+    {
+        light = GetComponent<Light>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		if ((GameObject.Find("Main Camera").GetComponent<LookScript>().lookingAtObject) == true)
+        {
+            light.intensity = 1.0f;
+        }
+        else
+        {
+            light.intensity = 0;
+        }
+	}
+}
