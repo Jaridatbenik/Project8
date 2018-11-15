febab10181443fb119adb9d6a7c62242908dd885
DESKTOP-MGQHNOV\Jari Kortier <JariKortier@gmail.com>
2018-11-15 11:52:18 +0100

    +circle handler dingetje

--- Assets/Collin/Script/TeleportHandler.cs
+++ Assets/Collin/Script/TeleportHandler.cs
@@ -2,20 +2,28 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TestScript : MonoBehaviour
+public class TeleportHandler : MonoBehaviour
 {
     Camera mcamera;
     public GameObject changelocation;
     Vector3 mouseposition = new Vector3();
 
+    CircleHandler circleHandler;
+
+    bool canTeleport = true;
+
     void Start()
     {
         mcamera = Camera.main;
+        circleHandler = GameObject.FindObjectOfType<CircleHandler>();
     }
 
     private void Update()
     {
-        Raycast();
+        if (canTeleport)
+        {
+            Raycast();
+        }
     }
 
     public void Raycast()
@@ -29,7 +37,14 @@ public class TestScript : MonoBehaviour
             if (Physics.Raycast(ray.origin, ray.direction, out hit, 20))
             {
                 changelocation.transform.position = hit.point;
+                canTeleport = false;
+                circleHandler.SpawnCircle(1.8f, CircleTypes.Teleportation, SetCanTeleportAgain, SetCanTeleportAgain);
             }
         }
+    }
+
+    void SetCanTeleportAgain()
+    {
+        canTeleport = true;
     }
 }
---
+++ Assets/SelectCircle/CircleHandler.cs
@@ -0,0 +1,187 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public enum CircleTypes
+{
+    Teleportation,
+    ObjectSelection
+}
+
+
+public class CircleHandler : MonoBehaviour
+{
+    public Image image;
+    float timer = 0;
+    float maxTime = 0;
+    CircleTypes currentType;
+    GameObject gameObjectIn;
+
+    bool inTimer = false;
+
+    //Temp-------------------
+    public GameObject cube;
+    //End Temp---------------
+
+    public void SpawnCircle(float timeToShow, CircleTypes type, Action subscribeToThisCompletionEvent, Action subscribeToThisCancelEvent)
+    {
+        this.maxTime = timeToShow;
+        this.currentType = type;
+        inTimer = true;
+
+        CircleEventHandler.SubscribeToEvent(subscribeToThisCompletionEvent, subscribeToThisCancelEvent);
+    }
+
+    public void SpawnCircle(float timeToShow, CircleTypes type, GameObject gameObj, Action subscribeToThisCompletionEvent, Action subscribeToThisCancelEvent)
+    {
+        this.maxTime = timeToShow;
+        this.currentType = type;
+        this.gameObjectIn = gameObj;
+        inTimer = true;
+
+        CircleEventHandler.SubscribeToEvent(subscribeToThisCompletionEvent, subscribeToThisCancelEvent);
+    }
+
+
+    void Start()
+    {
+        image.fillAmount = 0;
+    }
+
+    void Update()
+    {
+
+        //Temp---------------------------
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            CancelCircle();
+        }
+        if (Input.GetKeyDown(KeyCode.K))
+        {
+            SpawnCircle(0.2f, CircleTypes.ObjectSelection, cube, TestMethod, TestMethod);
+        }
+        //-------------------------------
+
+        if (inTimer)
+        {
+            if(timer <= maxTime)
+            {
+                timer += Time.deltaTime;
+                image.fillAmount = timer / maxTime;
+                if(currentType == CircleTypes.Teleportation)
+                {
+                    image.rectTransform.position = new Vector2(Screen.width / 2, Screen.height / 2);
+                }else
+                {
+                    image.rectTransform.position = Camera.main.WorldToScreenPoint(cube.transform.position);
+                }
+            }else
+            {
+                CancelCircle();
+                FinishCircle();
+            }
+        }
+    }
+
+    public void FinishCircle()
+    {
+        image.fillAmount = 0;
+        CircleEventHandler.RunEvents(true);
+    }
+
+    public void CancelCircle()
+    {
+        inTimer = false;
+        timer = 0;
+        maxTime = 0;
+        image.fillAmount = 0;
+        CircleEventHandler.RunEvents(false);
+    }
+
+    public void TestMethod()
+    {
+        Debug.Log("hoi");
+    }
+}
+
+public static class CircleEventHandler
+{ 
+    static List<Action> onComplete = new List<Action>();
+    static List<Action> onCancel = new List<Action>();
+
+    public static void SubscribeToEvent(Action complete, Action cancel)
+    {
+        onComplete.Add(complete);
+        onCancel.Add(cancel);
+    }
+
+    public static void UnsubscribeFromEvent(Action complete, Action cancel)
+    {
+        onComplete.Add(complete);
+        onCancel.Add(cancel);
+    }
+
+    /// <summary>
+    /// When you run this method, all methods you have subscribed to will be removed, if you dont want this put false as a paramater
+    /// Put in true if you want to run the complete events and false if you want to run the cancel events
+    /// </summary>
+    public static void RunEvents(bool completed)
+    {
+        if (completed)
+        {
+            for (int i = 0; i < onComplete.Count; i++)
+            {
+                onComplete[i].Invoke();
+            }
+
+            onComplete.Clear();
+        }else
+        {
+            for (int i = 0; i < onCancel.Count; i++)
+            {
+                onCancel[i].Invoke();
+            }
+
+            onCancel.Clear();
+        }
+    }
+
+    /// <summary>
+    /// When you run this method, all methods you have subscribed to will be removed, if you dont want this put false as a paramater
+    /// </summary>
+    public static void RunEvents(bool completed, bool boolin)
+    {
+        if (completed)
+        {
+            for (int i = 0; i < onComplete.Count; i++)
+            {
+                onComplete[i].Invoke();
+            }
+
+            if (boolin)
+            {
+                onComplete.Clear();
+            }
+        }else
+        {
+            for (int i = 0; i < onCancel.Count; i++)
+            {
+                onCancel[i].Invoke();
+            }
+
+            if (boolin)
+            {
+                onCancel.Clear();
+            }
+
+        }
+    }
+
+    /// <summary>
+    /// When you run this method nothing happens
+    /// </summary>
+    public static void NullableMethod() { }
+}
+
