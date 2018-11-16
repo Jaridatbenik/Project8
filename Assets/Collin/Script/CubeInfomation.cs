677e0c6b8a929c4027fd86a33108f94529119e6c
Collin Bannink <cococol2000@gmail.com>
2018-11-16 10:11:35 +0100

    * testing Ui info from gameobjects

---
+++ Assets/Collin/Script/CubeInfomation.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace CubeInfo
+{
+    public class CubeInfomation : MonoBehaviour
+    {
+        public float FloatInfo = 10f;
+        public string StringInfo = "Dit is een cube";
+
+
+    }
+}
---
+++ Assets/Collin/Script/TeleportHandler1.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using CubeInfo;
+
+public class TeleportHandler1 : MonoBehaviour
+{
+    Camera mcamera;
+    public GameObject changelocation;
+    Vector3 mouseposition = new Vector3();
+    public Text cubeText;
+
+    CubeInfomation cubeInfo = new CubeInfomation();
+
+    CircleHandler circleHandler;
+
+    bool canTeleport = true;
+
+    void Start()
+    {
+        mcamera = Camera.main;
+        circleHandler = GameObject.FindObjectOfType<CircleHandler>();
+    }
+
+    private void Update()
+    {
+        //if (canTeleport)
+        {
+            Raycast();
+        }
+    }
+
+    public void Raycast()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = mcamera.ScreenPointToRay(Input.mousePosition);
+
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray.origin, ray.direction, out hit, 20))
+            {
+                if(hit.transform.tag == "CubeInfo")
+                {
+                    cubeText.text = cubeInfo.StringInfo + "\n" + cubeInfo.FloatInfo.ToString();
+                    cubeText.rectTransform.position = Camera.main.WorldToScreenPoint(hit.transform.position + new Vector3(0, 50));
+                }
+
+                //changelocation.transform.position = hit.point;
+                //canTeleport = false;
+                //circleHandler.SpawnCircle(1.8f, CircleTypes.Teleportation, SetCanTeleportAgain, SetCanTeleportAgain);
+            }
+        }
+    }
+
+    void SetCanTeleportAgain()
+    {
+        canTeleport = true;
+    }
+}
