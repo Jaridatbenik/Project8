e62481573f0ebb4fd384927f5c67465da13d5ab0
Timur <Timur.d.sel074@gmail.com>
2018-11-26 12:03:29 +0100

    *objectsnapping

--- Assets/Scripts/ObjectMover.cs
+++ Assets/Scripts/ObjectMover.cs
@@ -4,22 +4,17 @@ using UnityEngine;
 
 public class ObjectMover : MonoBehaviour
 {
-    Renderer rend;
-    public Material finalMaterial;
-    public Material previousMaterial;
     public float timer;
     public Transform tempTrans;
     Vector3 holdVector;
 
     private void Start()
     {
-        rend = GetComponent<Renderer>();
         holdVector = gameObject.transform.localScale;
     }
 
     public void GoToObject(GameObject other)
     {
-        rend.sharedMaterial = finalMaterial;
         transform.position = other.transform.position;
         transform.parent = other.transform;
         transform.rotation = new Quaternion(0, 0, 0, 0);
