82ecca0226384e01d73bccd63d93f2d9bbfd9adc
Sep <SepHorsthuis@hotmail.com>
2018-11-29 10:10:09 +0100

    +Code input stuff

---
+++ Assets/Scripts/Robot/NumpadHandler.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class NumpadHandler : MonoBehaviour
+{
+    const int outputLimit = 4;
+    List<GameObject> pads = new List<GameObject>();
+    [SerializeField]
+    TextMeshProUGUI output;
+    [SerializeField]
+    GameObject previewText;
+
+    [SerializeField]ShakeUI textShaker;
+
+    [SerializeField] string code = "1234";
+
+    void Start()
+    {
+        for(int i = 0; i < transform.childCount; i++)
+        {
+            pads.Add(transform.GetChild(i).gameObject);
+        }         
+    }
+
+    public void ButtonPressed(int padIndex)
+    {
+        previewText.SetActive(false);
+        output.text += padIndex.ToString();
+
+        if (output.text.Length >= outputLimit)
+        {
+            CheckAnswer();
+        }
+    }
+
+    void CheckAnswer()
+    {
+        if (output.text == code)        
+            Correct();        
+        else
+            InCorrect();
+    }
+
+    void Correct()
+    {
+
+    }
+
+    void InCorrect()
+    {
+        textShaker.GoShake();
+        //maybe shake robot(?)
+    }
+
+    public void ClearText()
+    {
+        output.text = "";
+        previewText.SetActive(true);
+    }
+}
---
+++ Assets/Scripts/Robot/RobotHandler.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RobotHandler : MonoBehaviour
+{
+    [SerializeField]Animator roboParentAnim;
+    [SerializeField]
+    string faceAwayTrigger = "FaceAway";
+    [SerializeField]
+    string FaceTowardsTrigger = "FaceTowards";
+    
+    public void AllAttached()
+    {
+        roboParentAnim.SetTrigger(faceAwayTrigger);
+    }
+    public void CodeCorrect()
+    {
+        roboParentAnim.SetTrigger(FaceTowardsTrigger);
+    }
+
+    void Update()
+    {
+        //Debug
+        if(Input.GetKeyDown(KeyCode.R))
+        {
+            AllAttached();
+        }    
+        else if(Input.GetKeyDown(KeyCode.Q))
+        {
+            CodeCorrect();
+        }
+    }   
+}
