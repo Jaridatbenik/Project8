e15272c4b4984e1e9643fcd9bef27b2e2fb1c54a
Sep <SepHorsthuis@hotmail.com>
2018-12-14 09:40:57 +0100

    +finally pushed

---
+++ Assets/Scripts/Menu/MenuCanvas.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuCanvas : MonoBehaviour
+{
+    SubMenu countdownMenu;
+    Animator anim;
+    public Transform countDownText;
+       
+    public void EVENT_ToggleAnim()
+    {
+        if (countdownMenu == null)
+        {
+            countdownMenu = FindObjectOfType<CountDown>().GetComponent<SubMenu>();
+        }
+        countdownMenu.Toggle();
+    }
+}
---
+++ Assets/Scripts/OperatingSystem/General/ClearMenus.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClearMenus : MonoBehaviour
+{
+    [SerializeField]List<SubMenu> menus = new List<SubMenu>();  //temp serialized.
+
+    public void ClearAllMenus()
+    {        
+        if(menus.Count < FindObjectsOfType<SubMenu>().Length)
+        {
+            GetAllMenus();            
+        }
+        for (int i = 0; i < menus.Count; i++)
+        {            
+            if (menus[i].transform.localScale.x > 0.2f)
+            {
+                menus[i].Toggle();
+            }
+        }        
+    }
+
+    void Start()
+    {
+        GetAllMenus();    
+    }
+
+    void GetAllMenus()
+    {
+        for(int i = 0; i < FindObjectsOfType<SubMenu>().Length; i++)
+        {
+            SubMenu current = FindObjectsOfType<SubMenu>()[i];
+            menus.Add(current);
+        }
+    }
+}
